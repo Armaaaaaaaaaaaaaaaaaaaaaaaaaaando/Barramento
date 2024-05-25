06a833126e40dff22671208bfccbddf6d9a17f97
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2024-05-25 18:04:30 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,74 +1,83 @@
-#include <stdio.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include "/home/aluno/TEC499/LabExemples/auxFiles/address_map_arm.h"
+#include <stdio.h>            // Inclui a biblioteca padrão de entrada e saída
+#include <fcntl.h>            // Inclui definições para controle de arquivos
+#include <sys/mman.h>         // Inclui definições para memória mapeada
+#include "/home/aluno/TEC499/LabExemples/auxFiles/address_map_arm.h" // Inclui um arquivo de mapeamento de endereços específicos do hardware
 
-//test clone git
+#define DATA_B 0x70  // Define o endereço relativo para o registrador DATA_B
+#define DATA_A 0x80  // Define o endereço relativo para o registrador DATA_A
+#define START 0xc0   // Define o endereço relativo para o registrador START
 
-#define DATA_B 0x70
-#define DATA_A 0x80
-#define START 0xc0
-
+// Função principal
 int main(){
-    int cor = 0;
+    int cor = 0; // Inicializa a variável cor com 0
     while(1){
-        som(cor);
-        cor += 1;
-        //sleep(1);
+        som(cor); // Chama a função 'som' com o valor atual de cor
+        cor += 1; // Incrementa a variável cor
+        //sleep(1); // (Comentado) Espera por 1 segundo (não está sendo usado)
+        break; // Sai do loop após uma iteração
     }
-    return 0;
+    return 0; // Retorna 0 para indicar que o programa terminou corretamente
 }
 
+// Função para imprimir um sprite na tela
+void print_sprite(int* DATA_A_ptr, int* DATA_B_ptr, int registrador,  int sp, int sprite, int x, int y){
+    *DATA_A_ptr = registrador << 5; // Ajusta o registrador e armazena no endereço apontado por DATA_A_ptr
+    sp = sp << 29; // Ajusta o valor de sp (posição de 29 bits à esquerda)
+    x = x << 19; // Ajusta o valor de x (posição de 19 bits à esquerda)
+    y = y << 9;  // Ajusta o valor de y (posição de 9 bits à esquerda)
 
+    *DATA_B_ptr = sp | x | y | sprite; // Combina todos os valores e armazena no endereço apontado por DATA_B_ptr
+}
+
+// Função para imprimir um polígono na tela
+void print_poligono(int* DATA_A_ptr, int* DATA_B_ptr, int endereco,int x, int y, int tamanho, int vermelho, int verde, int azul, int forma, int opcode){
+    *DATA_A_ptr = endereco << 4 | opcode; // Ajusta o endereço e opcode e armazena no endereço apontado por DATA_A_ptr
+    *DATA_B_ptr = (forma << 31) | (azul << 28) | (verde << 25) | (vermelho << 22) | (tamanho << 18) | (y << 9) | x; // Combina todos os valores e armazena no endereço apontado por DATA_B_ptr
+}
+
+// Função que executa operações relacionadas ao som
 void som(int cor) {
-    volatile int *DATA_B_ptr, *DATA_A_ptr, *START_ptr; // Ponteiro de endereço virtual para os LEDs vermelhos
-    int fd = -1; // Usado para abrir /dev/mem
-    void *LW_virtual; // Endereços físicos para a ponte leve (light-weight bridge)
+    volatile int *DATA_B_ptr, *DATA_A_ptr, *START_ptr; // Declara ponteiros para registradores mapeados na memória
+    int fd = -1; // Descritor de arquivo inicializado para abrir /dev/mem
+    void *LW_virtual; // Ponteiro para a base do mapeamento de memória
 
-    // Abrir /dev/mem para dar acesso a endereços físicos
+    // Abre /dev/mem para acesso a memória física
     if ((fd = open("/dev/mem", (O_RDWR | O_SYNC))) == -1) {
         printf("ERROR: could not open \"/dev/mem\"...\n");
         return (-1);
     }
 
-    // Obter um mapeamento de endereços físicos para endereços virtuais
+    // Cria um mapeamento de memória
     LW_virtual = mmap(NULL, LW_BRIDGE_SPAN, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, LW_BRIDGE_BASE);
     if (LW_virtual == MAP_FAILED) {
         printf("ERROR: mmap() failed...\n");
         close(fd);
         return (-1);
     }
+
+    // Ajusta os ponteiros para os registradores mapeados
     DATA_A_ptr = (int *)(LW_virtual + DATA_A);
     DATA_B_ptr = (int *)(LW_virtual + DATA_B);
     START_ptr = (int *)(LW_virtual + START);
 
-    //abaixo adiciona sprite
-    *DATA_A_ptr = 0b000000000000000000000000100000;
-    *DATA_B_ptr = 0b100000111100000011110000000011;
+    // Variáveis auxiliares
+    int x = 20;
+    int y = 100;
 
-    //abaixo define cor do background
-    //*DATA_A_ptr = 0;
-   //*DATA_B_ptr = 100;
+    // Chama a função para imprimir um polígono na tela
+    print_poligono(DATA_A_ptr, DATA_B_ptr, 10, 20, 20, 2, 5, 0, 1, 1, 3);
 
-    //abaixo define poligono
-    //*DATA_A_ptr = 0b000000000000000000000000100011;
-    //*DATA_B_ptr = 0b1110101100000101001011000011100110;
-    
-    
+    // Inicia e para o registrador START
     *START_ptr = 1;
     *START_ptr = 0;
 
-
-    //
-
-
-    // Fechar o mapeamento de endereço virtual previamente aberto
+    // Desfaz o mapeamento de memória
     if (munmap(LW_virtual, LW_BRIDGE_SPAN) != 0) {
         printf("ERROR: munmap() failed...\n");
         return (-1);
     }
 
-    // Fechar /dev/mem para dar acesso a endereços físicos
+    // Fecha o descritor de arquivo /dev/mem
     close(fd);
 
     return 0;
