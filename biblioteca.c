9217ecb76ce7c0eac5622da2fba49134679dc67d
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2024-06-11 16:13:14 -0300

    :feat: adicionando nuvens

--- biblioteca.c
+++ biblioteca.c
@@ -264,21 +264,56 @@
             }
         }   
 
-
+        //flores abaixo
 
         print_sprite(fd, &dataA,&dataB, 1, 1, 400,4,3);
-
         print_sprite(fd, &dataA, &dataB, 1,90, 450,4,4);    
-
         print_sprite(fd, &dataA,&dataB, 1, 150, 410,4,5);
-
+        print_sprite(fd, &dataA,&dataB, 1, 130, 410,23,11);
         print_sprite(fd, &dataA,&dataB, 1, 200, 400,4,6);
-
         print_sprite(fd, &dataA,&dataB, 1, 250, 420,4,7);
-
         print_sprite(fd, &dataA,&dataB, 1, 300, 420,4,8); 
-
         print_sprite(fd, &dataA,&dataB, 1, 350, 420,4,9); 
+        print_sprite(fd, &dataA,&dataB, 1, 355, 450,4,10); 
+        print_sprite(fd, &dataA,&dataB, 1, 430, 439, 4,12); 
+        print_sprite(fd, &dataA,&dataB, 1, 500, 439, 4,13);
+
+
+        //pedras abaixo
+
+        print_sprite(fd, &dataA,&dataB, 1, 400, 450,23,11); 
+        print_sprite(fd, &dataA,&dataB, 1, 79, 400,23,15);  
+        print_sprite(fd, &dataA,&dataB, 1, 79, 400,23,15);  
+        print_sprite(fd, &dataA,&dataB, 1, 200, 450,23,17);
+
+
+        //aviao1
+        print_sprite(fd, &dataA,&dataB, 1, 100, 150,6,19); 
+        //aviao2
+        print_sprite(fd, &dataA,&dataB, 1, 270, 150,6,20); 
+        //laser 1
+        print_sprite(fd, &dataA,&dataB, 1, 130, 150,13,21);
+        //laser 2
+        print_sprite(fd, &dataA,&dataB, 1, 200, 150,13,22); 
+
+        //nuvem 1
+        print_quadrado(fd, &dataA,&dataB, 8, 100, 100, 2, 7, 7, 7);
+        print_quadrado(fd, &dataA,&dataB, 9, 80, 100, 1, 7, 7, 7);
+        print_quadrado(fd, &dataA,&dataB, 11, 120, 105, 1, 7, 7, 7);
+
+
+        //nuvem 2
+        print_quadrado(fd, &dataA,&dataB, 12, 200, 70, 2, 7, 7, 7);
+        print_quadrado(fd, &dataA,&dataB, 13, 180, 70, 1, 7, 7, 7);
+        print_quadrado(fd, &dataA,&dataB, 14, 220, 70, 1, 7, 7, 7);
+
+        //nuvem 3
+        print_quadrado(fd, &dataA,&dataB, 16, 335, 98, 1, 7, 7, 7);
+        print_quadrado(fd, &dataA,&dataB, 17, 350, 102, 1, 7, 7, 7);
+    
+        
+
+        
         
         // Buffer para armazenar a leitura do dispositivo
         char buffer[100];
