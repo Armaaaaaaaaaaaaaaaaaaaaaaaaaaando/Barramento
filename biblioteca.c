e7af53edc14698524f0ff28a42e0cbe43f326d8e
DiogoDSJ <107954198+DiogoDSJ@users.noreply.github.com>
2024-06-11 11:26:13 -0300

    feat: Imagem de arvores com SOL

--- biblioteca.c
+++ biblioteca.c
@@ -74,6 +74,15 @@ int verificar_cordenadas(int x, int y){
     return 1;
 }
 
+int verificar_cordenadas_poligono(int x, int y){
+    if(x > 511 | y > 479 | x < 0 | y < 0){
+        perror("Coodernadas fora dos limites");
+        return 0;
+    }
+    return 1;
+}
+
+
 int verificar_tamanho(int tamanho){
     if(tamanho > 15 | tamanho < 0){
         perror("Tamanho fora dos limites");
@@ -129,6 +138,7 @@ int editar_sprite(int fd, uint32_t* DATA_A_ptr, uint32_t* DATA_B_ptr, int endere
 int editar_bloco_background(int fd, uint32_t* DATA_A_ptr, uint32_t* DATA_B_ptr, int x, int y, int vermelho, int verde, int azul){
     char informacao[512];
     int numero_bloco = y * 80 + x;
+    printf("%d\n", numero_bloco);
     if(!verificar_cor(vermelho, verde, azul)) return 0;
     if(!verificar_cordenadas(x, y)) return 0;
     if(!verificar_bloco(numero_bloco)) return 0;
@@ -142,20 +152,20 @@ int editar_bloco_background(int fd, uint32_t* DATA_A_ptr, uint32_t* DATA_B_ptr, 
 int print_quadrado(int fd, uint32_t* DATA_A_ptr, uint32_t* DATA_B_ptr, int endereco, int x, int y, int tamanho, int vermelho, int verde, int azul){
     char informacao[512];
     if(!verificar_cor(vermelho, verde, azul)) return 0;
-    if(!verificar_cordenadas(x, y)) return 0;
+    if(!verificar_cordenadas_poligono(x, y)) return 0;
     if(!verificar_registrador(endereco)) return 0;
     if(!verificar_tamanho(tamanho)) return 0;
-    *DATA_A_ptr = endereco << 4 | 3; // Configura o valor de DATA_A
-    *DATA_B_ptr = (azul << 28) | (verde << 25) | (vermelho << 22) | (tamanho << 18) | (y << 9) | x; // Configura o valor de DATA_B
+    *DATA_A_ptr = endereco << 4 | 0b0011; // Configura o valor de DATA_A
+    *DATA_B_ptr = (0 << 31) | (azul << 28) | (verde << 25) | (vermelho << 22) | (tamanho << 18) | (y << 9) | x; // Configura o valor de DATA_B
     snprintf(informacao, 512, "%ud%u", *DATA_A_ptr, *DATA_B_ptr);
     escrever_no_arquivo(fd, DATA_A_ptr, DATA_B_ptr, informacao);
     return 1; // printou
 }
 
-void print_triangulo(int fd, uint32_t* DATA_A_ptr, uint32_t* DATA_B_ptr, int endereco, int x, int y, int tamanho, int vermelho, int verde, int azul){
+int print_triangulo(int fd, uint32_t* DATA_A_ptr, uint32_t* DATA_B_ptr, int endereco, int x, int y, int tamanho, int vermelho, int verde, int azul){
     char informacao[512];
     if(!verificar_cor(vermelho, verde, azul)) return 0;
-    if(!verificar_cordenadas(x, y)) return 0;
+    if(!verificar_cordenadas_poligono(x, y)) return 0;
     if(!verificar_registrador(endereco)) return 0;
     if(!verificar_tamanho(tamanho)) return 0;
     *DATA_A_ptr = endereco << 4 | 3; // Configura o valor de DATA_A
@@ -166,7 +176,7 @@ void print_triangulo(int fd, uint32_t* DATA_A_ptr, uint32_t* DATA_B_ptr, int end
 }
 
 // Função para configurar e imprimir um sprite
-void print_sprite(int fd, uint32_t* DATA_A_ptr, uint32_t* DATA_B_ptr, uint32_t ativar_sprite, int x, int y, int sprite, int registrador) {
+int print_sprite(int fd, uint32_t* DATA_A_ptr, uint32_t* DATA_B_ptr, uint32_t ativar_sprite, int x, int y, int sprite, int registrador) {
     char informacao[512];
     if(!verificar_sprite(sprite)) return 0;
     if(!verificar_cordenadas(x, y)) return 0;
@@ -178,7 +188,7 @@ void print_sprite(int fd, uint32_t* DATA_A_ptr, uint32_t* DATA_B_ptr, uint32_t a
     return 1; // printou
 }
 
-void set_background(int fd, uint32_t* DATA_A_ptr, uint32_t* DATA_B_ptr, int azul, int verde, int vermelho){
+int set_background(int fd, uint32_t* DATA_A_ptr, uint32_t* DATA_B_ptr, int vermelho, int verde, int azul){
     char informacao[512];
     if(!verificar_cor(vermelho, verde, azul)) return 0;
     *DATA_A_ptr = 0;
@@ -213,24 +223,46 @@ int main() {
     uint32_t dataA = 0;
     uint32_t dataB = 0;
 
-    // Chama a função para configurar um sprite
-    //print_poligono(fd, &dataA, &dataB, 10, 320, 240, 2, 5, 0, 1, 1, 3);
-    //print_sprite(fd, &dataA, &dataB, 1, 300, 200, 0, 2);
-    set_background(fd, &dataA,&dataB, 5, 5, 5);
-    printf("Colocou\n");
-    
-    //aviao
-    //print_poligono(fd, &dataA, &dataB, 10, 50, 200, 4, 5, 0, 1, 0, 3);
-    //print_sprite(&dataA, &dataB, 2, 300, 200, 2, 5);
-    /*
-    print_poligono(fd, &dataA, &dataB, 40, 150, 200, 4, 5, 0, 1, 0, 3, informacao);
-    print_poligono(fd, &dataA, &dataB, 30, 200, 200, 4, 5, 0, 1, 0, 3, informacao);
-    print_poligono(fd, &dataA, &dataB, 50, 230, 215, 1, 5, 0, 1, 0, 3, informacao);
-    print_poligono(fd, &dataA, &dataB, 60, 35, 165, 1, 5, 0, 1, 0, 3, informacao);
-    print_poligono(fd, &dataA, &dataB, 10, 50, 200, 4, 5, 0, 1, 0, 3, informacao);
-    print_poligono(fd, &dataA, &dataB, 20, 100, 200, 4, 5, 0, 1, 0, 3, informacao);
-    */
-
+    int x = 70;
+    int y = 50;
+    set_background(fd, &dataA,&dataB, 3, 5, 6);
+    //editar_bloco_background(fd, &dataA,&dataB, 0, 400, 3, 5, 6);
+    /*for(int i = 0; i < 80; i++){
+        for(int j = 0; j < 60; j++){
+            editar_bloco_background(fd, &dataA,&dataB, i, j, 3, 5, 6);
+        }
+    }*/
+    for(int i = 0; i < 80; i++){
+        for(int j = 50; j < 60; j++){
+            editar_bloco_background(fd, &dataA,&dataB, i, j, 0, 4, 0);
+            printf("i: %d, j: %d\n", i, j);
+        }
+    }
+    print_quadrado(fd, &dataA,&dataB, 10, 511, 100, 5, 5, 5, 0);
+    print_triangulo(fd, &dataA,&dataB, 2, 320, 280, 15, 3, 4, 2);
+    print_triangulo(fd, &dataA,&dataB, 4, 380, 280, 15, 3, 4, 2);
+    print_triangulo(fd, &dataA,&dataB, 5, 440, 280, 15, 3, 4, 2);
+    print_triangulo(fd, &dataA,&dataB, 6, 500, 280, 15, 3, 4, 2);
+    for(int i = 62; i < 65; i++){
+        for(int j = 45; j < 50; j++){
+            editar_bloco_background(fd, &dataA,&dataB, i, j, 4, 2, 1);
+        }
+    }
+    for(int i = 54; i < 57; i++){
+        for(int j = 45; j < 50; j++){
+            editar_bloco_background(fd, &dataA,&dataB, i, j, 4, 2, 1);
+        }
+    }
+    for(int i = 46; i < 49; i++){
+        for(int j = 45; j < 50; j++){
+            editar_bloco_background(fd, &dataA,&dataB, i, j, 4, 2, 1);
+        }
+    }
+    for(int i = 38; i < 41; i++){
+        for(int j = 45; j < 50; j++){
+            editar_bloco_background(fd, &dataA,&dataB, i, j, 4, 2, 1);
+        }
+    }
     // Buffer para armazenar a leitura do dispositivo
     char buffer[100];
     if (read_device(fd, buffer, sizeof(buffer)) < 0) {
