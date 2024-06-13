218b4b388b3f5ba09d36c91253f922b3400cbd9f
DiogoDSJ <107954198+DiogoDSJ@users.noreply.github.com>
2024-06-13 11:05:31 -0300

    refactor: Transferindo main da biblioteca.c para aqui.

--- Main.c
+++ Main.c
@@ -1,86 +1,123 @@
-#include <stdio.h>            // Inclui a biblioteca padrão de entrada e saída
-#include <fcntl.h>            // Inclui definições para controle de arquivos
-#include <sys/mman.h>         // Inclui definições para memória mapeada
-#include "/home/aluno/TEC499/LabExemples/auxFiles/address_map_arm.h" // Inclui um arquivo de mapeamento de endereços específicos do hardware
+#include "biblioteca.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <stdint.h>
+#include <inttypes.h>
 
-#define DATA_B 0x70  // Define o endereço relativo para o registrador DATA_B
-#define DATA_A 0x80  // Define o endereço relativo para o registrador DATA_A
-#define START 0xc0   // Define o endereço relativo para o registrador START
 
-// Função principal
-int main(){
-    int cor = 0; // Inicializa a variável cor com 0
-    while(1){
-        som(cor); // Chama a função 'som' com o valor atual de cor
-        cor += 1; // Incrementa a variável cor
-        //sleep(1); // (Comentado) Espera por 1 segundo (não está sendo usado)
-        break; // Sai do loop após uma iteração
+    int main() {
+        const char *device_path = "/dev/driver_dos_amigos"; // Substitua pelo caminho do seu dispositivo
+        int fd = open_device(device_path); // Abre o dispositivo
+        char informacao[512]; // Buffer para armazenar informações
+        uint32_t dataA = 0;
+        uint32_t dataB = 0;
+
+        int x = 70;
+        int y = 50;
+        set_background(fd, &dataA,&dataB, 3, 5, 6);
+        //editar_bloco_background(fd, &dataA,&dataB, 0, 400, 3, 5, 6);
+        /*for(int i = 0; i < 80; i++){
+            for(int j = 0; j < 60; j++){
+                editar_bloco_background(fd, &dataA,&dataB, i, j, 3, 5, 6);
+            }
+        }*/ // fundo da cor do fundo
+        for(int i = 0; i < 80; i++){
+            for(int j = 50; j < 60; j++){
+                editar_bloco_background(fd, &dataA,&dataB, i, j, 0, 4, 0);
+            }
+        }
+        print_quadrado(fd, &dataA,&dataB, 10, 511, 100, 5, 5, 5, 0);
+        print_triangulo(fd, &dataA,&dataB, 2, 320, 280, 15, 3, 4, 2);
+        print_triangulo(fd, &dataA,&dataB, 4, 380, 280, 15, 3, 4, 2);
+        print_triangulo(fd, &dataA,&dataB, 5, 440, 280, 15, 3, 4, 2);
+        print_triangulo(fd, &dataA,&dataB, 6, 500, 280, 15, 3, 4, 2);
+        for(int i = 62; i < 65; i++){
+            for(int j = 45; j < 50; j++){
+                editar_bloco_background(fd, &dataA,&dataB, i, j, 4, 2, 1);
+            }
+        }
+        for(int i = 54; i < 57; i++){
+            for(int j = 45; j < 50; j++){
+                editar_bloco_background(fd, &dataA,&dataB, i, j, 4, 2, 1);
+            }
+        }
+        for(int i = 46; i < 49; i++){
+            for(int j = 45; j < 50; j++){
+                editar_bloco_background(fd, &dataA,&dataB, i, j, 4, 2, 1);
+            }
+        }
+        for(int i = 38; i < 41; i++){
+            for(int j = 45; j < 50; j++){
+                editar_bloco_background(fd, &dataA,&dataB, i, j, 4, 2, 1);
+            }
+        }   
+
+        //flores abaixo
+
+        print_sprite(fd, &dataA,&dataB, 1, 1, 400,4,3);
+        print_sprite(fd, &dataA, &dataB, 1,90, 450,4,4);    
+        print_sprite(fd, &dataA,&dataB, 1, 150, 410,4,5);
+        print_sprite(fd, &dataA,&dataB, 1, 130, 410,23,11);
+        print_sprite(fd, &dataA,&dataB, 1, 200, 400,4,6);
+        print_sprite(fd, &dataA,&dataB, 1, 250, 420,4,7);
+        print_sprite(fd, &dataA,&dataB, 1, 300, 420,4,8); 
+        print_sprite(fd, &dataA,&dataB, 1, 350, 420,4,9); 
+        print_sprite(fd, &dataA,&dataB, 1, 355, 450,4,10); 
+        print_sprite(fd, &dataA,&dataB, 1, 430, 439, 4,12); 
+        print_sprite(fd, &dataA,&dataB, 1, 500, 439, 4,13);
+
+
+        //pedras abaixo
+
+        print_sprite(fd, &dataA,&dataB, 1, 400, 450,23,11); 
+        print_sprite(fd, &dataA,&dataB, 1, 79, 400,23,15);  
+        print_sprite(fd, &dataA,&dataB, 1, 79, 400,23,15);  
+        print_sprite(fd, &dataA,&dataB, 1, 200, 450,23,17);
+
+
+        //aviao1
+        print_sprite(fd, &dataA,&dataB, 1, 100, 150,6,19); 
+        //aviao2
+        print_sprite(fd, &dataA,&dataB, 1, 270, 150,6,20); 
+        //laser 1
+        print_sprite(fd, &dataA,&dataB, 1, 130, 150,13,21);
+        //laser 2
+        print_sprite(fd, &dataA,&dataB, 1, 200, 150,13,22); 
+
+        //nuvem 1
+        print_quadrado(fd, &dataA,&dataB, 8, 100, 100, 2, 7, 7, 7);
+        print_quadrado(fd, &dataA,&dataB, 9, 80, 100, 1, 7, 7, 7);
+        print_quadrado(fd, &dataA,&dataB, 11, 120, 105, 1, 7, 7, 7);
+
+
+        //nuvem 2
+        print_quadrado(fd, &dataA,&dataB, 12, 200, 70, 2, 7, 7, 7);
+        print_quadrado(fd, &dataA,&dataB, 13, 180, 70, 1, 7, 7, 7);
+        print_quadrado(fd, &dataA,&dataB, 14, 220, 70, 1, 7, 7, 7);
+
+        //nuvem 3
+        print_quadrado(fd, &dataA,&dataB, 16, 335, 98, 1, 7, 7, 7);
+        print_quadrado(fd, &dataA,&dataB, 17, 350, 102, 1, 7, 7, 7);
+    
+        
+
+        
+        
+        // Buffer para armazenar a leitura do dispositivo
+        char buffer[100];
+        if (read_device(fd, buffer, sizeof(buffer)) < 0) {
+            close_device(fd); // Fecha o dispositivo se falhar
+            return EXIT_FAILURE;
+        }
+
+        // Fecha o dispositivo
+        close_device(fd); 
+        return EXIT_SUCCESS;
     }
-    return 0; // Retorna 0 para indicar que o programa terminou corretamente
-}
 
-// Função para imprimir um sprite na tela
-void print_sprite(int* DATA_A_ptr, int* DATA_B_ptr, int registrador,  int sp, int sprite, int x, int y){
-    *DATA_A_ptr = registrador << 5; // Ajusta o registrador e armazena no endereço apontado por DATA_A_ptr
-    sp = sp << 29; // Ajusta o valor de sp (posição de 29 bits à esquerda)
-    x = x << 19; // Ajusta o valor de x (posição de 19 bits à esquerda)
-    y = y << 9;  // Ajusta o valor de y (posição de 9 bits à esquerda)
-
-    *DATA_B_ptr = sp | x | y | sprite; // Combina todos os valores e armazena no endereço apontado por DATA_B_ptr
-}
-
-// Função para imprimir um polígono na tela
-void print_poligono(int* DATA_A_ptr, int* DATA_B_ptr, int endereco,int x, int y, int tamanho, int vermelho, int verde, int azul, int forma, int opcode){
-    *DATA_A_ptr = endereco << 4 | opcode; // Ajusta o endereço e opcode e armazena no endereço apontado por DATA_A_ptr
-    *DATA_B_ptr = (forma << 31) | (azul << 28) | (verde << 25) | (vermelho << 22) | (tamanho << 18) | (y << 9) | x; // Combina todos os valores e armazena no endereço apontado por DATA_B_ptr
-}
-
-// Função que executa operações relacionadas ao som
-void som(int cor) {
-    volatile int *DATA_B_ptr, *DATA_A_ptr, *START_ptr; // Declara ponteiros para registradores mapeados na memória
-    int fd = -1; // Descritor de arquivo inicializado para abrir /dev/mem
-    void *LW_virtual; // Ponteiro para a base do mapeamento de memória
-
-    // Abre /dev/mem para acesso a memória física
-    if ((fd = open("/dev/mem", (O_RDWR | O_SYNC))) == -1) {
-        printf("ERROR: could not open \"/dev/mem\"...\n");
-        return (-1);
-    }
-
-    // Cria um mapeamento de memória
-    LW_virtual = mmap(NULL, LW_BRIDGE_SPAN, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, LW_BRIDGE_BASE);
-    if (LW_virtual == MAP_FAILED) {
-        printf("ERROR: mmap() failed...\n");
-        close(fd);
-        return (-1);
-    }
-
-    // Ajusta os ponteiros para os registradores mapeados
-    DATA_A_ptr = (int *)(LW_virtual + DATA_A);
-    DATA_B_ptr = (int *)(LW_virtual + DATA_B);
-    START_ptr = (int *)(LW_virtual + START);
-
-    // Variáveis auxiliares
-    int x = 20;
-    int y = 100;
-
-    // Chama a função para imprimir um polígono na tela
-    print_poligono(DATA_A_ptr, DATA_B_ptr, 10, 20, 20, 2, 5, 0, 1, 1, 3);
-
-    // Inicia e para o registrador START
-    *START_ptr = 1;
-    *START_ptr = 0;
-
-    // Desfaz o mapeamento de memória
-    if (munmap(LW_virtual, LW_BRIDGE_SPAN) != 0) {
-        printf("ERROR: munmap() failed...\n");
-        return (-1);
-    }
-
-    // Fecha o descritor de arquivo /dev/mem
-    close(fd);
-
-    return 0;
-}
 
 
